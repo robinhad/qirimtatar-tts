093ff4b04bf70a63d6bc9652e364f5e723abc88a
Yurii Paniv <mr.robinhad@gmail.com>
2022-11-17 21:26:09 +0200

    Fix tests

--- tests/test_preprocessor.py
+++ tests/test_preprocessor.py
@@ -3,5 +3,5 @@ from crh_preprocessor.preprocessor import preprocess
 
 def test_preprocessor():
     assert (
-        preprocess("İşanç Alla-Taalâğa.") == "işanç alla-taalâğa."
+        preprocess("İşanç Alla-Taalâğa.") == "işan\u04ab alla-taalâğa."
     )  # first i is two symbols (i without dot and dot)
